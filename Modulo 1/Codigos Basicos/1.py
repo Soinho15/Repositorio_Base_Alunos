c05f820542171b2ead5b4fa08719be4ff8066a77
Soinho15 <josemarcelosn15@gmail.com>
2025-09-10 09:22:34 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/1.py
@@ -0,0 +1,5 @@
+numero = int(input("Digite um numero para ver a tabuada: "))
+lista=[1,2,3,4,5,6,7,8,9,10]
+
+for item in lista:
+  print(   numero * item)
---
+++ Modulo 1/Codigos Basicos/3.py
@@ -0,0 +1,8 @@
+palavra = input("Digite uma palavra")
+
+
+vogais = ("a,e,i,o,")
+for item in palavra:
+    if item in vogais:
+        print("Quantidade de vogais:")
+
